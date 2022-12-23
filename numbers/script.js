503751c4c880fde79916eff389fcc5dd422aaf0b
tomthoene <106812884+tomthoene@users.noreply.github.com>
2022-12-23 18:54:05 +0100

    Reset

--- numbers/script.js
+++ numbers/script.js
@@ -1,64 +1,69 @@
-let maxNumber = 100
-let bits = maxNumber.toString(2).length
-let card = 0
-let result = 0
-let numbers = []
-let firstNumber = 0
+let maxNumber = 100;
+let bits = maxNumber.toString(2).length;
+let card = 0;
+let result = 0;
+let numbers = [];
+let firstNumber = 0;
 
-start()
+document.getElementById("text").innerHTML = "Denke an eine Zahl zwischen 0 und " + maxNumber
+refreshControls(true, true);
 
-function start () {
-    refreshControls(false)
-    card = 0
-    result = 0
-    next()
+function start() {
+  refreshControls(false, false)
+  card = 0;
+  result = 0;
+  next(false);
+  document.getElementById("h1").innerHTML = "Ist deine Zahl hier?";
 }
-function next (yes) { 
-    let numbers = []
-    card ++
-    if (card > bits) {
-        document.getElementById("text").innerHTML = result;
-        document.getElementById("h1").innerHTML = "Ergebnis:";
-        refreshControls(true)
-        return
+function next(yes) {
+  let numbers = [];
+  card++;
+  if (card > bits) {
+    document.getElementById("text").innerHTML = result;
+    document.getElementById("h1").innerHTML = "Ergebnis:";
+    refreshControls(true, false);
+    return;
+  }
+
+  for (let i = 1; i <= maxNumber; i++) {
+    if (toBinary(i, card) == 1) {
+      numbers.push(i);
     }
-    
-    for (let i = 1; i <= maxNumber; i ++) {
-        if (toBinary(i, card) == 1) {
-            numbers.push(i)
-        }
-    }
-   
-    console.log(result)
-    console.log(numbers)
-    document.getElementById("text").innerHTML = numbers.join(" ");
-    firstNumber = numbers[0]
+  }
+  document.getElementById("text").innerHTML = numbers.join(" ");
+  firstNumber = numbers[0];
 }
-function yes () {
-    result += firstNumber
-    next(true)
+function yes() {
+  result += firstNumber;
+  next(true);
 }
-function no () {
-    next(false)
+function no() {
+  next(false);
 }
-function toBinary (input, bitIndex) {
-    let binary = input.toString(2) 
-    return binary[binary.length - bitIndex];
+function toBinary(input, bitIndex) {
+  let binary = input.toString(2);
+  return binary[binary.length - bitIndex];
 }
 
-function refreshControls (refresh) {
-    const buttons = document.getElementsByClassName("controls")
-    const refreshButton = document.getElementById("refresh")
-    for (button of buttons) {
-            if (refresh) {
-                button.style.display = "none"
-            } else {
-                button.style.display = "block"
-            }
-        }
+function refreshControls(refresh, start) {
+  const buttons = document.getElementsByClassName("controls");
+  const refreshButton = document.getElementById("refresh");
+  //const symbol = document.getElementById("symbol");
+  for (button of buttons) {
     if (refresh) {
-        refreshButton.style.display = "block"
+      button.style.display = "none";
     } else {
-        refreshButton.style.display = "none"
+      button.style.display = "block";
     }
+  }
+  if (refresh) {
+    refreshButton.style.display = "block";
+    if (start) {
+      symbol.innerHTML = " play_arrow ";
+    } else {
+      symbol.innerHTML = " refresh ";
+    }
+  } else {
+    refreshButton.style.display = "none";
+  }
 }
